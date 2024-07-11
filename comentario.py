1979a5fd91112845e01a682cd73df8185b930232
Luise Jáuregui <38704851+luisejv@users.noreply.github.com>
2024-07-11 08:12:31 -0500

    Add files via upload

---
+++ comentario.py
@@ -0,0 +1,29 @@
+import boto3
+import uuid
+import os
+
+def lambda_handler(event, context):
+    # Entrada (json)
+    print(event)
+    tenant_id = event['body']['tenant_id']
+    texto = event['body']['texto']
+    nombre_tabla = os.environ["TABLE_NAME"]
+    # Proceso
+    uuidv1 = str(uuid.uuid1())
+    comentario = {
+        'tenant_id': tenant_id,
+        'uuid': uuidv1,
+        'detalle': {
+          'texto': texto
+        }
+    }
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(nombre_tabla)
+    response = table.put_item(Item=comentario)
+    # Salida (json)
+    print(comentario)
+    return {
+        'statusCode': 200,
+        'comentario': comentario,
+        'response': response
+    }
